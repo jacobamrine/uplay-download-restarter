285332052a5eb305fc986e7d7e8f647e7db23d48
Jacob Amrine <64110198+jacobamrine@users.noreply.github.com>
2026-05-06 23:45:12 -0600

    modernize comments

--- dlrestarter.py
+++ dlrestarter.py
@@ -1,5 +1,4 @@
-#Jacob Amrine 3/7/20
-#This program checks if Uplay stops downloading, and restarts the program if that is the case
+#This program checks if Uplay stops downloading and restarts the program after a short delay
 
 import pyautogui
 import os
@@ -7,12 +6,13 @@ import time
 import datetime
 
 running = True
-interval = 600 #Interval between while loop checks
+interval = 600 #Interval between checks
 
 DLFails = 0 #Number of download failures
 DLThreshold = 20 #Number of download failures to end program.
 iconFailures = 0 #Number of icon failures. Resets to 0 once the icon is found
 iconThreshold = 100 #Number of failures locatin the icon before exiting the while loop
+UPlayInstallDir = "E:/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/Uplay.exe"
 
 print("This was started at " + str(datetime.datetime.now()))
 while running:
@@ -24,26 +24,22 @@ while running:
         DLFails = DLFails + 1
         print("Number of failures: " + str(DLFails))
         if DLFails > DLThreshold:
-            print("DOWNLOAD FAILURE EXCEEDED THRESHOLD. PROGRAM WILL TERMINATE.")
+            print("Reached failure threshold. Program will terminate")
             running = False
             break #Something better?
         os.system("taskkill /IM upc.exe") #Kill Uplay program and wait
         time.sleep(600)
-        os.system('"E:/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/Uplay.exe"') #Restart Uplay
+        os.system(UPlayInstallDir) #Restart Uplay
         dlIcon = pyautogui.locateOnScreen('DLICON.PNG')
         while dlIcon == None:
             print("Couldnt locate DLICON")
             time.sleep(30)
             dlIcon = pyautogui.locateOnScreen('DLICON.PNG')
             iconFailures = iconFailures + 1
-            #tryAgainButton = pyautogui.locateOnScreen('TRYAGAIN.PNG')
-            #if tryAgainButton != None:
-                #a, b = x, y = pyautogui.locateCenterOnScreen('TRYAGAIN.PNG')
-                #+pyautogui.click(a, b)
             if iconFailures > iconThreshold:
                 dlIcon = 1
                 running = False
-                print("ICON FAILURE EXCEEDED THRESHOLD. Program will likely error below.")
+                print("Icon failure exceeded threshold.")
                 break
         iconFailures = 0
         x, y = pyautogui.locateCenterOnScreen('DLICON.PNG')
